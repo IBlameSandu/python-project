70e6a210e64606580e2356105c4d73291a9d1587
IBlameSandu <127618863+TheOneAugust@users.noreply.github.com>
2024-06-06 12:38:05 +0200

    af

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     for x in range(5):
         lijsten[f"Partij{x+1}"]=Lijst(f"Partij{x+1}")
 
-    print("partijen aangemaakt.")
+    print("partijen werden aangemaakt.")
 
     #Kiezers en kandidaten aanmaken, de kandidaten worden ook direct in hun partij gezet.
     count=1
@@ -25,9 +25,9 @@ def main():
             Kiezers[f"kiezer{x}"]=kandidaat
         else:
             Kiezers[f"kiezer{x}"]=Kiezer(x)
-    print("kiezers en kandidaten aangemaakt.")
+    print("kiezers en kandidaten werden aangemaakt.")
     
-    print(f"werkt volledig, kiezer lengte {len(Kiezers)}, 50 kandidaten")
+    #print(f"werkt volledig, kiezer lengte {len(Kiezers)}, 50 kandidaten")
 
     usb=USBStick()
     scann=Scanner()
@@ -36,7 +36,7 @@ def main():
     stemcomputer1=Stemcomputer(usb)
     stemcomputer2=Stemcomputer(usb)
     stemcomputer3=Stemcomputer(usb)
-    #print("stemcomputers en usb aangemaakt")
+    print("stemcomputers, usb, stembus en scanner aangemaakt!")
     #nummer=0
     with open("output.txt", "w") as output:
         for kiezer in Kiezers.values():
@@ -45,18 +45,30 @@ def main():
                 chipkaarten[f"chipkaart{x}"] = Chipkaart()
             randomNummer=math.floor(random.random()*60)
             chipkaartVanDeKiezer=chipkaarten[f"chipkaart{randomNummer}"]
+
+            output.write(f"60 chipkaarten werden geïnitialiseerd\n")
             output.write(f"kiezer {kiezer.getName()} kreeg chipkaart {randomNummer} met opstartcode {chipkaartVanDeKiezer.getOpstart()}\n")
+
             #nummer+=60
             stemcomputer1.stemmen(kiezer, chipkaartVanDeKiezer, lijsten)
             stemcomputer2.stemmen(kiezer, chipkaartVanDeKiezer, lijsten)
             stemcomputer3.stemmen(kiezer, chipkaartVanDeKiezer, lijsten)
+
+            output.write(f"kiezer {kiezer.getName()} heeft zijn stem ingevoerd\n")
+
             if kiezer.heeftGestemd():
                 stem=stemcomputer3.geefStembil(kiezer)
-                output.write(f"{kiezer.getName()} heeft succesvol gestemd!\n")
+                output.write(f"kiezer {kiezer.getName()} zijn stem wordt gevalideerd!\n")
+
                 scann.check(stem)
                 stembus.addStembil(stem)
-    output.close()
+                output.write(f"{kiezer.getName()} heeft succesvol gestemd!\n\n")
+
+    output.close() #voor de zekerheid
+
     parser(lijsten)
+    print("een tekst bestand met de volledige output is nu beschikbaar!")
+    print("de HTML output van de verkiezingen is nu beschikbaar!")
     #for lijst in lijsten.values():
         #for x in lijst.getKandidaten():
             #print(f"kandidaat {x.getName()} heeft {x.getStemmen()} stemmen")
